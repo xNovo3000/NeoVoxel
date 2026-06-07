5e463cb18e4c75adf24f5a14578b2639ff2efe4d
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-06-07 17:55:47 +0200

    Implemented windows main and tracing into entrypoint

--- engine/include/neovoxel/entrypoint.hpp
+++ engine/include/neovoxel/entrypoint.hpp
@@ -8,12 +8,23 @@
 #include <neovoxel/application.hpp>
 #include <neovoxel/debug.hpp>
 
+#if defined(_WIN32)
+    #include <Windows.h>
+#endif
+
 extern neovoxel::application *create_application();
 
-int main(int argc, char const *argv[]) {
+#if NV_BUILD_TYPE == NV_BUILD_TYPE_RELEASE && defined(_WIN32)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nShowCmd)
+#else
+int main(int argc, char const *argv[])
+#endif
+{
     neovoxel::logging::initialize();
+    NV_TRACING_BEGIN();
     auto app = create_application();
     app->run();
     delete app;
+    NV_TRACING_END();
     return 0;
 }
