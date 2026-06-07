3d53f93ecbe4735cdf87756bc8f264055b65f593
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-06-07 17:09:04 +0200

    Finished context implementation

--- engine/include/neovoxel/context.hpp
+++ engine/include/neovoxel/context.hpp
@@ -17,7 +17,12 @@ namespace neovoxel {
         window(const char *_name);
         virtual ~window() = default;
 
-        virtual std::vector<event_ptr> pull_events();
+        // Non-copyable but movable, required for smart pointers
+        window(const window&) = delete;
+        window(window&&) noexcept = default;
+        window &operator=(window&&) noexcept = default;
+
+        virtual std::vector<event_ptr> poll_events();
         virtual void swap_buffers();
 
         virtual void title(const char *_title);
@@ -41,10 +46,19 @@ namespace neovoxel {
         input(const char *_name);
         virtual ~input() = default;
 
+        // Non-copyable but movable, required for smart pointers
+        input(const input&) = delete;
+        input(input&&) noexcept = default;
+        input &operator=(input&&) noexcept = default;
+
         virtual std::vector<event_ptr> poll_events();
 
         virtual void cursor_mode(input_cursor_mode _mode);
         virtual input_cursor_mode cursor_mode() const;
+
+        virtual glm::vec2 cursor_position() const;
+        virtual bool key_pressed() const;
+        virtual bool mouse_button_pressed() const;
 
     };
 
--- engine/src/neovoxel/context.cpp
+++ engine/src/neovoxel/context.cpp
@@ -6,7 +6,7 @@ namespace neovoxel {
     window::window() : window("window") {}
     window::window(const char *_name) : named_resource(_name) {}
 
-    std::vector<event_ptr> window::pull_events() { return {}; }
+    std::vector<event_ptr> window::poll_events() { return {}; }
     void window::swap_buffers() {}
 
     void window::title(const char *_title) {}
@@ -16,5 +16,17 @@ namespace neovoxel {
 
     void window::refresh_rate(uint32_t _rate) {}
     uint32_t window::refresh_rate() const { return 0; }
+
+    input::input() : input("input") {}
+    input::input(const char *_name) : named_resource(_name) {}
+
+    std::vector<event_ptr> input::poll_events() { return {}; }
+
+    void input::cursor_mode(input_cursor_mode _mode) {}
+    input_cursor_mode input::cursor_mode() const { return input_cursor_mode::normal; }
+
+    glm::vec2 input::cursor_position() const { return {}; }
+    bool input::key_pressed() const { return false; }
+    bool input::mouse_button_pressed() const { return false; }
 
 }
