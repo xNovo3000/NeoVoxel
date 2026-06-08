ea67d03407af0c8b98cf561f2571b1867fc3d636
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-06-09 00:01:01 +0200

    Started adding functions to application

--- engine/include/neovoxel/application.hpp
+++ engine/include/neovoxel/application.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
 #include <neovoxel/core.hpp>
+#include <neovoxel/context.hpp>
+#include <neovoxel/event.hpp>
+#include <neovoxel/layer.hpp>
+
+#include <vector>
 
 namespace neovoxel {
 
@@ -15,6 +20,18 @@ namespace neovoxel {
     private:
         static application *_instance;
 
+        bool _is_running;  // False only when termination is requested
+        timepoint _last_tick_time;  // The last timepoint of the tick
+
+        /* Layer Stack */
+        std::vector<layer_ptr> _layer_stack;
+        std::vector<layer*> _layer_insert_queue;
+        std::vector<layer*> _layer_delete_queue;
+
+        /* Context, graphics, audio and multithreading */
+        window_ptr _window;
+        input_ptr _input;
+
     public:
         static application &instance() { return *_instance; }
 
@@ -27,6 +44,13 @@ namespace neovoxel {
         application(application&&) noexcept = delete;
 
         void run();
+
+        virtual void push_layer(layer *_layer);
+        virtual void pop_layer(layer *_layer);
+        virtual void terminate();
+
+        window &get_window() const noexcept { return *_window; }
+        input &get_input() const noexcept { return *_input; }
 
     };
 
--- engine/src/neovoxel/application.cpp
+++ engine/src/neovoxel/application.cpp
@@ -9,7 +9,11 @@ namespace neovoxel {
 
     application::application() : application("application") {}
 
-    application::application(const char *_name) : named_resource(_name) {
+    application::application(const char *_name) : named_resource(_name),
+        _is_running(true), _last_tick_time(0),
+        _layer_stack(), _layer_insert_queue(), _layer_delete_queue(),
+        _window(nullptr), _input(nullptr)
+    {
         NV_LOG_INFO("Application '{}': created", name());
         _instance = this;
     }
@@ -21,7 +25,15 @@ namespace neovoxel {
     
     void application::run() {
         NV_LOG_INFO("Application '{}': started", name());
+        while (_is_running) {
+            NV_TRACING_WATCH;
+        }
         NV_LOG_INFO("Application '{}': stopped", name());
+    }
+
+    void application::terminate() {
+        NV_LOG_INFO("Application '{}': termination requested");
+        _is_running = false;
     }
 
 }
