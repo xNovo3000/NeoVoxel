876b4edd0d4a1927070e387c85d8665cd60086b2
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-06-18 20:35:51 +0200

    Added gpu_buffer

--- engine/include/neovoxel/graphics.hpp
+++ engine/include/neovoxel/graphics.hpp
@@ -5,8 +5,44 @@
 #include <glm/glm.hpp>
 
 #include <memory>
+#include <optional>
+#include <vector>
 
 namespace neovoxel {
+
+    enum class gpu_buffer_draw_type { _static, dynamic };
+    enum class gpu_buffer_element {
+        vec1_i8,  vec2_i8,  vec3_i8,  vec4_i8 ,
+        vec1_u8,  vec2_u8,  vec3_u8,  vec4_u8 ,
+        vec1_i16, vec2_i16, vec3_i16, vec4_i16,
+        vec1_u16, vec2_u16, vec3_u16, vec4_u16,
+        vec1_i32, vec2_i32, vec3_i32, vec4_i32,
+        vec1_u32, vec2_u32, vec3_u32, vec4_u32,
+        vec1_f32, vec2_f32, vec3_f32, vec4_f32
+    };
+
+    struct gpu_buffer_spec {
+        std::vector<std::vector<gpu_buffer_element>> _buffers;
+        gpu_buffer_draw_type _draw_type;
+    };
+
+    class gpu_buffer {
+
+    private:
+        std::optional<uint32_t> _handle;
+
+    public:
+        explicit gpu_buffer(const gpu_buffer_spec &_spec);
+        ~gpu_buffer();
+
+        // Non-copyable but movable, required for smart pointers
+        gpu_buffer(const gpu_buffer&) = delete;
+        gpu_buffer(gpu_buffer&&) noexcept = default;
+        gpu_buffer &operator=(gpu_buffer&&) noexcept = default;
+
+    };
+
+    using gpu_buffer_ref = std::shared_ptr<gpu_buffer>;
 
 #if NV_BUILD_TYPE == NV_BUILD_TYPE_DEBUG
     constexpr glm::vec4 _graphics_default_clear_color { 0.75F, 0.125F, 0.65F, 0.0F };
@@ -29,6 +65,8 @@ namespace neovoxel {
         graphics_api(const graphics_api&) = delete;
         graphics_api(graphics_api&&) noexcept = default;
         graphics_api &operator=(graphics_api&&) noexcept = default;
+
+        static gpu_buffer_ref create(const gpu_buffer_spec &_spec);
 
         virtual void clear_color(const glm::vec4 &_color = _graphics_default_clear_color);
         virtual void clear_depth(double _depth = _graphics_default_clear_depth);
--- engine/src/neovoxel/graphics.cpp
+++ engine/src/neovoxel/graphics.cpp
@@ -3,8 +3,19 @@
 
 namespace neovoxel {
 
+    /* gpu_buffer */
+
+    gpu_buffer::gpu_buffer(const gpu_buffer_spec &_spec) : _handle() {}
+    gpu_buffer::~gpu_buffer() {}
+
+    /* graphics_api */
+
     graphics_api::graphics_api() : graphics_api("graphics_api") {}
     graphics_api::graphics_api(const char *_name) : named_resource(_name) {}
+
+    gpu_buffer_ref graphics_api::create(const gpu_buffer_spec &_spec) {
+        return std::make_shared<gpu_buffer>(_spec);
+    }
 
     void graphics_api::clear_color(const glm::vec4 &_color) {}
     void graphics_api::clear_depth(double _depth) {}
