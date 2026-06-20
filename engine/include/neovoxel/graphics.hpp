cc902417f5eb1112a79c83c1011ee79ce00b3b7f
xNovo3000 <fontana.ciro.2001@gmail.com>
2026-06-20 12:03:07 +0200

    Finished buffer and added shader

--- engine/include/neovoxel/graphics.hpp
+++ engine/include/neovoxel/graphics.hpp
@@ -6,16 +6,18 @@
 
 #include <memory>
 #include <optional>
+#include <string>
 #include <vector>
 
 namespace neovoxel {
 
     enum class gpu_buffer_draw_type { _static, dynamic };
     enum class gpu_buffer_element {
-        vec1_i8,  vec2_i8,  vec3_i8,  vec4_i8 ,
-        vec1_u8,  vec2_u8,  vec3_u8,  vec4_u8 ,
-        vec1_i16, vec2_i16, vec3_i16, vec4_i16,
-        vec1_u16, vec2_u16, vec3_u16, vec4_u16,
+// TODO: This buffer types will be implemented later
+//      vec1_i8,  vec2_i8,  vec3_i8,  vec4_i8 ,
+//      vec1_u8,  vec2_u8,  vec3_u8,  vec4_u8 ,
+//      vec1_i16, vec2_i16, vec3_i16, vec4_i16,
+//      vec1_u16, vec2_u16, vec3_u16, vec4_u16,
         vec1_i32, vec2_i32, vec3_i32, vec4_i32,
         vec1_u32, vec2_u32, vec3_u32, vec4_u32,
         vec1_f32, vec2_f32, vec3_f32, vec4_f32
@@ -40,9 +42,42 @@ namespace neovoxel {
         gpu_buffer(gpu_buffer&&) noexcept = default;
         gpu_buffer &operator=(gpu_buffer&&) noexcept = default;
 
+        void draw() const;
+
+        template <typename T> void set_vertex_data(uint32_t _index, const std::vector<T> &_data);
+        template <typename T> void set_vertex_subdata(uint32_t _index, uint32_t _offset, const std::vector<T> &_data);
+        template <typename T> void set_index_data(const std::vector<T> &_data);
+
     };
 
     using gpu_buffer_ref = std::shared_ptr<gpu_buffer>;
+
+    struct gpu_shader_spec {
+        std::string _vertex_shader_code;
+        std::string _fragment_shader_code;
+    };
+
+    class gpu_shader {
+
+    private:
+        std::optional<uint32_t> _handle;
+
+    public:
+        explicit gpu_shader(const gpu_shader_spec &_spec);
+        ~gpu_shader();
+
+        // Non-copyable but movable, required for smart pointers
+        gpu_shader(const gpu_shader&) = delete;
+        gpu_shader(gpu_shader&&) noexcept = default;
+        gpu_shader &operator=(gpu_shader&&) noexcept = default;
+
+        void use() const;
+
+        template <typename T> void set_uniform(const std::string &_name, const T &_value);
+
+    };
+
+    using gpu_shader_ref = std::shared_ptr<gpu_shader>;
 
 #if NV_BUILD_TYPE == NV_BUILD_TYPE_DEBUG
     constexpr glm::vec4 _graphics_default_clear_color { 0.75F, 0.125F, 0.65F, 0.0F };
@@ -67,6 +102,7 @@ namespace neovoxel {
         graphics_api &operator=(graphics_api&&) noexcept = default;
 
         static gpu_buffer_ref create(const gpu_buffer_spec &_spec);
+        static gpu_shader_ref create(const gpu_shader_spec &_spec);
 
         virtual void clear_color(const glm::vec4 &_color = _graphics_default_clear_color);
         virtual void clear_depth(double _depth = _graphics_default_clear_depth);
@@ -76,6 +112,29 @@ namespace neovoxel {
         virtual void enable(graphics_capability _capability);
 
         virtual void set_viewport(const glm::ivec2 &_size);
+    
+    protected:
+        virtual uint32_t _gb_create(const gpu_buffer_spec &_spec);
+        virtual void _gb_destroy(uint32_t _handle);
+        virtual void _gb_draw(uint32_t _handle);
+        virtual void _gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<int32_t> &_data);
+        virtual void _gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<uint32_t> &_data);
+        virtual void _gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<float> &_data);
+        virtual void _gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<int32_t> &_data);
+        virtual void _gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<uint32_t> &_data);
+        virtual void _gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<float> &_data);
+        virtual void _gb_set_index_data(uint32_t _handle, const std::vector<uint16_t> &_data);
+        virtual void _gb_set_index_data(uint32_t _handle, const std::vector<uint32_t> &_data);
+        friend class gpu_buffer;
+
+        virtual uint32_t _gs_create(const gpu_shader_spec &_spec);
+        virtual void _gs_destroy(uint32_t _handle);
+        virtual void _gs_set_uniform(uint32_t _handle, const std::string &_name, const int32_t &_value);
+        virtual void _gs_set_uniform(uint32_t _handle, const std::string &_name, const uint32_t &_value);
+        virtual void _gs_set_uniform(uint32_t _handle, const std::string &_name, const float &_value);
+        virtual void _gs_set_uniform(uint32_t _handle, const std::string &_name, const glm::mat3 &_value);
+        virtual void _gs_set_uniform(uint32_t _handle, const std::string &_name, const glm::mat4 &_value);
+        friend class gpu_shader;
 
     };
 
--- engine/src/neovoxel/graphics.cpp
+++ engine/src/neovoxel/graphics.cpp
@@ -1,12 +1,88 @@
 #include <pch.hpp>
 #include <neovoxel/graphics.hpp>
 
+#include <neovoxel/application.hpp>
+#include <neovoxel/debug.hpp>
+
 namespace neovoxel {
 
     /* gpu_buffer */
 
-    gpu_buffer::gpu_buffer(const gpu_buffer_spec &_spec) : _handle() {}
-    gpu_buffer::~gpu_buffer() {}
+    gpu_buffer::gpu_buffer(const gpu_buffer_spec &_spec) :
+        _handle(application::get().get_graphics_api().graphics_api::_gb_create(_spec))
+    {}
+
+    gpu_buffer::~gpu_buffer() {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gb_destroy(*_handle);
+    }
+
+    void gpu_buffer::draw() const {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gb_draw(*_handle);
+        else
+            NV_LOG_WARN("gpu_buffer:draw() called with empty handle");
+    }
+
+    template <typename T>
+    void gpu_buffer::set_vertex_data(uint32_t _index, const std::vector<T> &_data) {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gb_set_vertex_data(*_handle, _index, _data);
+        else
+            NV_LOG_WARN("gpu_buffer:set_vertex_data() called with empty handle");
+    }
+
+    template <> void gpu_buffer::set_vertex_data(uint32_t _index, const std::vector<int32_t> &_data);
+    template <> void gpu_buffer::set_vertex_data(uint32_t _index, const std::vector<uint32_t> &_data);
+    template <> void gpu_buffer::set_vertex_data(uint32_t _index, const std::vector<float> &_data);
+
+    template <typename T>
+    void gpu_buffer::set_vertex_subdata(uint32_t _index, uint32_t _offset, const std::vector<T> &_data) {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gb_set_vertex_subdata(*_handle, _index, _offset, _data);
+        else
+            NV_LOG_WARN("gpu_buffer:set_vertex_subdata() called with empty handle");
+    }
+
+    template <> void gpu_buffer::set_vertex_subdata(uint32_t _index, uint32_t _offset, const std::vector<int32_t> &_data);
+    template <> void gpu_buffer::set_vertex_subdata(uint32_t _index, uint32_t _offset, const std::vector<uint32_t> &_data);
+    template <> void gpu_buffer::set_vertex_subdata(uint32_t _index, uint32_t _offset, const std::vector<float> &_data);
+
+    template <typename T>
+    void gpu_buffer::set_index_data(const std::vector<T> &_data) {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gb_set_index_data(*_handle, _data);
+        else
+            NV_LOG_WARN("gpu_buffer:set_index_data() called with empty handle");
+    }
+
+    template <> void gpu_buffer::set_index_data(const std::vector<uint16_t> &_data);
+    template <> void gpu_buffer::set_index_data(const std::vector<uint32_t> &_data);
+
+    /* gpu_shader */
+
+    gpu_shader::gpu_shader(const gpu_shader_spec &_spec) :
+        _handle(application::get().get_graphics_api().graphics_api::graphics_api::_gs_create(_spec))
+    {}
+
+    gpu_shader::~gpu_shader() {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::graphics_api::_gs_destroy(*_handle);
+    }
+
+    template <typename T>
+    void gpu_shader::set_uniform(const std::string &_name, const T &_value) {
+        if (_handle) [[likely]]
+            application::get().get_graphics_api().graphics_api::_gs_set_uniform(*_handle, _name, _value);
+        else
+            NV_LOG_WARN("gpu_buffer:set_index_data() called with empty handle");
+    }
+
+    template <> void gpu_shader::set_uniform(const std::string &_name, const int32_t &_value);
+    template <> void gpu_shader::set_uniform(const std::string &_name, const uint32_t &_value);
+    template <> void gpu_shader::set_uniform(const std::string &_name, const float &_value);
+    template <> void gpu_shader::set_uniform(const std::string &_name, const glm::mat3 &_value);
+    template <> void gpu_shader::set_uniform(const std::string &_name, const glm::mat4 &_value);
 
     /* graphics_api */
 
@@ -17,6 +93,10 @@ namespace neovoxel {
         return std::make_shared<gpu_buffer>(_spec);
     }
 
+    gpu_shader_ref graphics_api::create(const gpu_shader_spec &_spec) {
+        return std::make_shared<gpu_shader>(_spec);
+    }
+
     void graphics_api::clear_color(const glm::vec4 &_color) {}
     void graphics_api::clear_depth(double _depth) {}
     void graphics_api::clear_stencil(int32_t _stencil) {}
@@ -25,5 +105,25 @@ namespace neovoxel {
     void graphics_api::enable(graphics_capability _capability) {}
 
     void graphics_api::set_viewport(const glm::ivec2 &_size) {}
+
+    uint32_t graphics_api::_gb_create(const gpu_buffer_spec &_spec) { return 0; }
+    void graphics_api::_gb_destroy(uint32_t _handle) {}
+    void graphics_api::_gb_draw(uint32_t _handle) {}
+    void graphics_api::_gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<int32_t> &_data) {}
+    void graphics_api::_gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<uint32_t> &_data) {}
+    void graphics_api::_gb_set_vertex_data(uint32_t _handle, uint32_t _index, const std::vector<float> &_data) {}
+    void graphics_api::_gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<int32_t> &_data) {}
+    void graphics_api::_gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<uint32_t> &_data) {}
+    void graphics_api::_gb_set_vertex_subdata(uint32_t _handle, uint32_t _index, uint32_t _offset, const std::vector<float> &_data) {}
+    void graphics_api::_gb_set_index_data(uint32_t _handle, const std::vector<uint16_t> &_data) {}
+    void graphics_api::_gb_set_index_data(uint32_t _handle, const std::vector<uint32_t> &_data) {}
+
+    uint32_t graphics_api::_gs_create(const gpu_shader_spec &_spec) { return 0; }
+    void graphics_api::_gs_destroy(uint32_t _handle) {}
+    void graphics_api::_gs_set_uniform(uint32_t _handle, const std::string &_name, const int32_t &_value) {}
+    void graphics_api::_gs_set_uniform(uint32_t _handle, const std::string &_name, const uint32_t &_value) {}
+    void graphics_api::_gs_set_uniform(uint32_t _handle, const std::string &_name, const float &_value) {}
+    void graphics_api::_gs_set_uniform(uint32_t _handle, const std::string &_name, const glm::mat3 &_value) {}
+    void graphics_api::_gs_set_uniform(uint32_t _handle, const std::string &_name, const glm::mat4 &_value) {}
 
 }
